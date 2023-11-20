68972662c02faa6642cce44eadeb1382c4c3b69c
Mo Ahmad <moaahmad@gmail.com>
2023-11-20 09:46:01 +0000

    Points to latest merged changes on main

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(
             name: "Segment",
             url: "https://github.com/reveri-health/analytics-swift-flush-fix",
-            branch: "bsneed/operatingMode"
+            branch: "main"
         ),
         // NOTE: The repackaging of the firebase sdk results in a mismatch in name and a duplicate firebase package dependency without the deprecated named version
         // NOTE: There may be a workaround for this to fix the identity between the app dependency and the analytics dependency but skipping research for time
