56d7095f3ea77b816f06346c53cf0923eb85efa2
Tristan Warner-Smith <tristan@reveri.com>
2024-02-29 15:10:39 +0000

    Bumps analytics-swift to 1.5.5 and Firebase to 10.21

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SegmentFirebase",
-            targets: ["SegmentFirebase"]),
+            targets: ["SegmentFirebase"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,26 +24,28 @@ let package = Package(
         .package(
             name: "Segment",
             url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.4.7"
+            from: "1.5.5"
         ),
-		.package(
-			name: "Firebase",
-			url: "https://github.com/firebase/firebase-ios-sdk",
-			from: "10.21.0"
-		)
+        .package(
+            name: "Firebase",
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            from: "10.21.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentFirebase",
-            dependencies: ["Segment",
-                            .product(
-                                name: "FirebaseAnalytics",
-                                package: "Firebase")
-                          ]
+            dependencies: [
+                "Segment",
+                .product(
+                    name: "FirebaseAnalytics",
+                    package: "Firebase"
+                )
+            ]
         ),
-        
+
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
     ]
 )
